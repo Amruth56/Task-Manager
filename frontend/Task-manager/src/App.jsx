13ee4e8a93a296c3ac55f06ea3260b3c03904253
Amruth <ammumandappa@gmail.com>
2025-11-06 18:22:14 +0530

    created empty components

---
+++ frontend/Task-manager/src/App.jsx
@@ -0,0 +1,11 @@
+import {
+  BrowserRouter as Router, Routes, Route
+} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className='text-3xl'>App</div>
+  )
+}
+
+export default App
