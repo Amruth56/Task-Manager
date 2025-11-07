bb1db1956a33992a6eb60454da04cbe8263b69ff
Amruth <ammumandappa@gmail.com>
2025-11-07 19:57:15 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 // Middleware to handle CORS
-app.arguments(
+app.use(
     cors({
         origin: process.env.CLIENT_URL || "*",
         methods: ["GET", "POST", "PUT", "DELETE"],
@@ -14,4 +14,21 @@ app.arguments(
     })
 );
 
-// 
+// Middleware
+app.use(express.json());
+
+// Routes 
+// app.use("api/auth", authRoutes);
+// app.use("api/users", userRoutes);
+// app.use("api/tasks", taskRoutes);
+// app.use("api/reports", reportRoutes);
+
+
+
+// Start Server 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+
