bebafdab7aa052a702c0b4807c09d9a740ac9297
Amruth <ammumandappa@gmail.com>
2025-11-09 16:44:17 +0530

    created jwt verification function

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -17,4 +17,25 @@ export default async function authMiddleware(req, res, next){
     const token = authHeader.split(' ')[1];
 
     // verify and attach user object
+    try{
+        const payload = jwt.verify(token , JWT_SECRET);
+        const user = await User.findById(payload.id).select('-password');
+
+        if(!user){
+            return res.status(401).json({
+                success: false,
+                message: "user not found"
+            })
+        }
+
+        req.user = user;
+        next();
+
+    }catch(err){
+        console.log("JWT verification failed");
+        return res.status(401).json({
+            succesS: false, 
+            message: "Token invalid or expired"
+        })
+    }
 }
